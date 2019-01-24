f21b3c0e6af5f8e0f4537a8c9b400f8adb47264b
Keith Turner <turner@keithturner.com>
2019-01-23 21:36:43 -0700

    Initial commit

---
+++ hello.py
@@ -0,0 +1,12 @@
+# Clears the screen every time you run a program in terminal/
+import os
+os.system('clear')
+
+# Prints Hello World to the screen
+print ('Hello World')
+
+# Prints  dashes ------ to the screen
+print ('--------------------')
+
+first_name = ('Winston')
+print(first_name)
