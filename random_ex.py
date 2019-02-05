13c270fcdd8d0c55d310a1aeb63ba3ad4a366067
Keith Turner <turner@keithturner.com>
2019-02-05 06:08:16 -0700

    Update rock app

--- random_ex.py
+++ random_ex.py
@@ -1,3 +1,3 @@
 import random
 
-print (random.randint(1,42))
+print (random.randint(0,42))
---
+++ rock.py
@@ -0,0 +1,7 @@
+# rock, paper, scissors
+
+import random
+
+random_choice = random.randint(0,2)
+
+print ('The computer chooses', random_choice)
