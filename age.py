78735efe5cc9a342ef3039ae8f8f4db6e5d3a463
Keith Turner <turner@keithturner.com>
2019-02-08 04:45:14 -0700

    Update rock app

--- age.py
+++ age.py
@@ -6,6 +6,6 @@ myName = input()
 print ('It is good to meet you, ' + myName)
 print ('The length of your name is:')
 print (len(myName))
-print ('What is your age?') # Asks for age
+print ("What's your age?") # Asks for age
 myAge = input()
 print ('You will be ' + str(int(myAge) + 1) + ' on your next birthday!')
