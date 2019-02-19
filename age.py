a18235431bb2266f638ef64cd188db695c63c9a0
Keith Turner <turner@keithturner.com>
2019-02-19 02:53:14 -0700

    Update rock app

--- age.py
+++ age.py
@@ -1,5 +1,5 @@
 # This program says hello and asks the user's name and age.
-# Prompt: 9753485
+# Prompt: 4746088
 print ('Hello')
 print ('What is your name?') # Asks for your name.
 myName = input()
