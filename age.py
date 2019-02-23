9ff594377287693253d0222678ab5c841320bca9
Keith Turner <turner@keithturner.com>
2019-02-23 02:31:51 -0700

    Update sequence

--- age.py
+++ age.py
@@ -1,5 +1,5 @@
 # This program says hello and asks the user's name and age.
-# Prompt: 8574583
+# Prompt: 5746497
 print ('Hello')
 print ('What is your name?') # Asks for your name.
 myName = input()
